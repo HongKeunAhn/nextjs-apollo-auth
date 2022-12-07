37bc68864d76603e1b8c4f71fba264ee4ff7d62a
Keu.ni <skcor8909@gmail.com>
2022-12-07 13:04:41 +0900

    <chore> add apollo-client

---
+++ constants/apolloClient.ts
@@ -0,0 +1,12 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { API_URL } from './urls';
+
+const httpLink = new HttpLink({
+  uri: `${API_URL}/graphql`,
+  credentials: 'same-origin',
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink,
+});
